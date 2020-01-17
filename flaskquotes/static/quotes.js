ec62eb8300d62c02ea605da734c1d298de3dd1c7
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-01-17 17:13:34 -0500

    Get new random quote client side. Initial quote is formatted server side.

--- flaskquotes/static/quotes.js
+++ flaskquotes/static/quotes.js
@@ -1,22 +1,37 @@
+var quote;
+var movie;
+var year;
+var rank = Math.floor(Math.random() * 100);
+
 function getNewQuote() {
     fade()
-    // TODO get new quote
+    quote = quotes.rank.Quote;
+    movie = quotes.rank.Movie;
+    year = quotes.rank.Year;
     var twitterLink = "https://twitter.com/intent/tweet?text=\"" +
-    linkQuote + "\"%20%20%20&hashtags=" + linkMovie + ",freeCodeCamp" +
+    quote + "\"%20%20%20&hashtags=" + movie + ",freeCodeCamp" +
     ",FrontEndWebDev"
     $("#tweet-link").attr("href", twitterLink);
+    var newIndex = Math.floor(Math.random() * 100);
+    if (newIndex != rank) {
+        rank = newIndex;
+    }
+    else {
+        newIndex = Math.floor(Math.randon() * 100);
+    }
 }
 
 function fade() {
+    formatted = "#" + rank + " -  " + quote + " - from " + movie + " (year)";
     $(".quote").fadeOut(1000, function () {
-        $(".quote").html("\"" + quote + "\"");
+        $(".quote").html("\"" + formatted + "\"");
         $(this).fadeIn(1000);
     });
 
-    $(".movie").fadeOut(1000, function () {
+    /*$(".movie").fadeOut(1000, function () {
         $(".movie").html("- " + movie);
         $(this).fadeIn(1000);
-    });
+    });*/
 
     $(".btn-basic").fadeOut(1000, function () {
         $(this).fadeIn(1000);
