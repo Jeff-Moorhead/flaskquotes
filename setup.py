b77c5b0e60ec7b1c9e1ee72fe7d3e040048a37b7
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-06-25 16:42:24 -0400

    Add long description

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name="flaskquotes",
       description="Get AFI top 100 quotes",
-      long_description=None,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       entry_points={
           'console_scripts': [
               'quotes=flaskquotes.console:main'
